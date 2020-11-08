b40dacd6272d31a74ba025bf107ec7b388f2f40b
rsahi2020 <rohit_sahi@hotmail.com>
2020-11-08 18:32:50 +0530

    Adding two comments in 2.py which will be deleted

--- python/2.py
+++ python/2.py
@@ -1,3 +1,6 @@
+<<<<<<< HEAD
 print("This file is updated on git hub")
 print ("Changes from git hub not acceptable") 
 print ("will be removing git hub changes")
+=======
+>>>>>>> parent of 64e45e9... Update 2.py
